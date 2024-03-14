7d2ec59d7748f8ad6ec93495d308cd7949a04219
admin1334 <jeetbherwani2004@gmail.com>
2024-03-14 09:25:49 +0530

    Responsive changes : Changes on displaying chat on mobile screens

--- src/components/Chat/DisplayChat.jsx
+++ src/components/Chat/DisplayChat.jsx
@@ -36,11 +36,11 @@ function DisplayChat() {
                         if(message['role'] === 'user') {
                             return(
                                 <div key={message['id']} className="bottom-up w-full flex items-start justify-start py-1 gap-3 md:py-2">
-                                    <div className="text-[16px] md:text-xl w-[25px] h-[25px] md:w-[1.9rem] md:h-[1.9rem] mr-1 rounded-full border-2 flex items-center justify-center">
+                                    <div className="text-[16px] md:text-xl w-[22px] h-[23px] md:w-[1.9rem] md:h-[1.9rem] mr-1 rounded-full border-2 flex items-center justify-center">
                                         <i className="lni lni-user m-5"></i>
                                     </div>
                                     <div>
-                                        <p className="text-gray-300 text-[16px] md:text-[18px] leading-[1.36rem]">{message['text']}</p>
+                                        <p className="text-gray-300 text-[15px] md:text-[18px] leading-[1.36rem]">{message['text']}</p>
                                     </div>
                                 </div>
                             )
@@ -48,11 +48,11 @@ function DisplayChat() {
                         if(message['role'] === 'ai' && message['id'] !== 13340) {
                             return (
                                 <div key={message['id']} className="relative w-full md:w-[900px] flex items-start justify-start gap-3 py-2 pb-6 md:py-3 md:pb-10">
-                                    <div className="text-lg md:text-10xl w-[1.9rem] h-[1.9rem] flex items-center justify-center">
+                                    <div className="text-[16px] md:text-10xl w-[1.6rem] h-[1.9rem] flex items-center justify-center">
                                         <img src="https://www.gstatic.com/lamda/images/gemini_sparkle_v002_d4735304ff6292a690345.svg" className="inline w-full h-full m-5" alt="" />
                                     </div>
                                     <div>
-                                        <p className="text-gray-300 whitespace-pre-line text-[16px] md:text-[18px] leading-[1.36rem]" dangerouslySetInnerHTML={{__html: message['text']}}/>
+                                        <p className="text-gray-300 whitespace-pre-line text-[15px] md:text-[18px] leading-[1.36rem]" dangerouslySetInnerHTML={{__html: message['text']}}/>
                                     </div>
                                 </div>
                             )
@@ -76,11 +76,11 @@ function DisplayChat() {
                         else if(message['role'] === 'ai-start') {
                             return (
                                 <div key={message['id']} className="max-w-full flex items-start justify-center gap-3 py-2 pb-6 md:py-3 md:pb-9">
-                                    <div className="text-lg md:text-10xl w-[1.9rem] h-[1.9rem] flex items-center justify-center">
+                                    <div className="text-lg md:text-10xl w-[1.6rem] h-[1.9rem] flex items-center justify-center">
                                         <img src="https://www.gstatic.com/lamda/images/gemini_sparkle_v002_d4735304ff6292a690345.svg" className="inline w-full h-full m-5" alt="" />
                                     </div>
                                     <div className="relative">
-                                        <p className="text-gray-300 whitespace-pre-line text-[16px] md:text-[18px]">Hello, how can I assist you today?</p>
+                                        <p className="text-gray-300 whitespace-pre-line text-[15px] md:text-[18px]">Hello, how can I assist you today?</p>
                                     </div>
                                 </div>
                             )
